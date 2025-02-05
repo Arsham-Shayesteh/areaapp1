2fefe20746fddf60b75caecda712588825c93624
ACER <ACER@DESKTOP-H8R5HI8>
2025-02-04 21:31:26 -0800

    Add project files.

---
+++ areaapp1/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using areaapp1.Models;
+using System.Web.Security;
+
+namespace areaapp1.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login(string returnUrl)
+        {
+
+            LoginModel loginModel = new LoginModel()
+            {
+                ReturnUrl = returnUrl,
+            };
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login([Bind(Include ="UserName,Password,ReturnUrl")] LoginModel user)
+        {
+            if(ModelState.IsValid)
+            {
+                if(user.UserName == "mehdi" && user.Password =="123")
+                {
+                   FormsAuthentication.SetAuthCookie(user.UserName,false);
+                    return Redirect(user.ReturnUrl);
+                }
+                return View();
+            }
+            return View();
+        }
+    }
+}
---
+++ areaapp1/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace areaapp1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult step1()
+        {
+            return View();
+        }
+
+    }
+}
